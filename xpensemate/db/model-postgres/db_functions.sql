ed136512692c3be5a35f06baa10dca1929a19e56
Dana Christen <dana.christen@gmail.com>
2014-10-30 14:44:31 -0700

    Improved the readability of the SQL file containing the DB functions

--- xpensemate/db/model-postgres/db_functions.sql
+++ xpensemate/db/model-postgres/db_functions.sql
@@ -23,30 +23,39 @@
 --
 
 
---- List the members of a group with their overall balance in the group
+-- List the members of a group with their overall balance in the group
 CREATE OR REPLACE FUNCTION get_user(name VARCHAR)
     RETURNS SETOF table_member AS
     $BODY$
-        SELECT id, name, ENCODE(password_hash, 'base64'), ENCODE(password_salt, 'base64'), active
+        SELECT
+            id,
+            name,
+            ENCODE(password_hash, 'base64'),
+            ENCODE(password_salt, 'base64'),
+            active
         FROM table_member
-        WHERE name = $1 AND active IS TRUE
+        WHERE name = $1
+            AND active IS TRUE
     $BODY$
     LANGUAGE 'sql';
 
 
---- List the groups of a given member, based on a member id
+-- List the groups of a given member, based on a member id
 CREATE OR REPLACE FUNCTION get_groups(member_id INTEGER)
     RETURNS SETOF table_group AS
     $BODY$
-        SELECT table_group.id AS group_id, table_group.name AS group_name
+        SELECT
+            table_group.id AS group_id,
+            table_group.name AS group_name
         FROM table_member_group
-        INNER JOIN table_group ON table_group.id = table_member_group.group_id
+        INNER JOIN table_group
+            ON table_group.id = table_member_group.group_id
         WHERE table_member_group.member_id = $1
     $BODY$
     LANGUAGE 'sql';
 
 
------ List the groups of a given member, based on a member name
+---- List the groups of a given member, based on a member name
 --CREATE OR REPLACE FUNCTION get_groups(member_name VARCHAR)
     --RETURNS SETOF table_group AS
     --$BODY$
@@ -59,21 +68,24 @@ CREATE OR REPLACE FUNCTION get_groups(member_id INTEGER)
     --LANGUAGE 'sql';
 
 
---- List the members of a group, based on a group id
+-- List the members of a group, based on a group id
 DROP TYPE IF EXISTS member_t CASCADE;
 CREATE TYPE member_t AS (id INTEGER, name VARCHAR);
 CREATE OR REPLACE FUNCTION get_group_members(group_id INTEGER)
     RETURNS SETOF member_t AS
     $BODY$
-        SELECT table_member.id AS member_id, table_member.name AS member_name
+        SELECT
+            table_member.id AS member_id,
+            table_member.name AS member_name
         FROM table_member_group
-        INNER JOIN table_member ON table_member_group.member_id = table_member.id
+        INNER JOIN table_member
+            ON table_member_group.member_id = table_member.id
         WHERE table_member_group.group_id = $1
     $BODY$
     LANGUAGE 'sql';
 
 
------ List the members of a group, based on a group name
+---- List the members of a group, based on a group name
 --CREATE OR REPLACE FUNCTION get_group_members(group_name VARCHAR)
     --RETURNS SETOF member_t AS
     --$BODY$
@@ -86,98 +98,146 @@ CREATE OR REPLACE FUNCTION get_group_members(group_id INTEGER)
     --LANGUAGE 'sql';
 
 
---- List the expenses of a group, based on a group_id
+-- List the expenses of a group, based on a group_id
 DROP TYPE IF EXISTS expense_t CASCADE;
-CREATE TYPE expense_t AS (expense_id INTEGER, date_info DATE, description VARCHAR, amount NUMERIC, expense_maker VARCHAR, expense_members VARCHAR);
+CREATE TYPE expense_t AS (
+    expense_id INTEGER,
+    date_info DATE,
+    description VARCHAR,
+    amount NUMERIC,
+    expense_maker VARCHAR,
+    expense_members VARCHAR);
+    
 CREATE OR REPLACE FUNCTION get_group_expenses(group_id INTEGER)
     RETURNS SETOF expense_t AS
     $BODY$
-        SELECT table_expense.id, table_expense.date_info, table_expense.description, table_expense.amount, expense_maker_name.name AS expense_maker, STRING_AGG(expense_members_names.name, '|') AS expense_members
+        SELECT
+            table_expense.id,
+            table_expense.date_info,
+            table_expense.description,
+            table_expense.amount,
+            expense_maker_name.name AS expense_maker,
+            STRING_AGG(expense_members_names.name, '|') AS expense_members
         FROM table_expense
-        INNER JOIN table_expense_member expense_members ON table_expense.id = expense_members.expense_id
-        INNER JOIN table_member expense_members_names ON expense_members_names.id = expense_members.member_id
-        INNER JOIN table_expense_member expense_maker ON table_expense.id = expense_maker.expense_id AND expense_maker.made_expense IS TRUE
-        INNER JOIN table_member expense_maker_name ON expense_maker_name.id = expense_maker.member_id
+        INNER JOIN table_expense_member expense_members
+            ON table_expense.id = expense_members.expense_id
+        INNER JOIN table_member expense_members_names
+            ON expense_members_names.id = expense_members.member_id
+        INNER JOIN table_expense_member expense_maker
+            ON table_expense.id = expense_maker.expense_id
+                AND expense_maker.made_expense IS TRUE
+        INNER JOIN table_member expense_maker_name
+            ON expense_maker_name.id = expense_maker.member_id
         WHERE table_expense.group_id = $1
-        GROUP BY table_expense.id, table_expense.date_info, table_expense.description, table_expense.amount, expense_maker_name.name
+        GROUP BY
+            table_expense.id,
+            table_expense.date_info,
+            table_expense.description,
+            table_expense.amount,
+            expense_maker_name.name
         ORDER BY table_expense.date_info ASC
     $BODY$
     LANGUAGE 'sql';
 
 
---- List the transfers of a group
+-- List the transfers of a group
 DROP TYPE IF EXISTS transfer_t CASCADE;
-CREATE TYPE transfer_t AS (id INTEGER, date_info DATE, amount NUMERIC, from_member VARCHAR, to_member VARCHAR);
+CREATE TYPE transfer_t AS (
+    id INTEGER,
+    date_info DATE,
+    amount NUMERIC,
+    from_member VARCHAR,
+    to_member VARCHAR);
+    
 CREATE OR REPLACE FUNCTION get_group_transfers(group_id INTEGER)
     RETURNS SETOF transfer_t AS
     $BODY$
-        SELECT table_transfer.id, table_transfer.date_info, table_transfer.amount, from_members_lookup.name, to_members_lookup.name
+        SELECT
+            table_transfer.id,
+            table_transfer.date_info,
+            table_transfer.amount,
+            from_members_lookup.name,
+            to_members_lookup.name
         FROM table_transfer
-        INNER JOIN table_member from_members_lookup ON from_members_lookup.id = table_transfer.from_member_id
-        INNER JOIN table_member to_members_lookup ON to_members_lookup.id = table_transfer.to_member_id
+        INNER JOIN table_member from_members_lookup
+            ON from_members_lookup.id = table_transfer.from_member_id
+        INNER JOIN table_member to_members_lookup
+            ON to_members_lookup.id = table_transfer.to_member_id
         WHERE table_transfer.group_id=$1
         ORDER BY table_transfer.date_info ASC
     $BODY$
     LANGUAGE 'sql';
 
 
---- Get the balance of a given member in a given group
+-- Get the balance of a given member in a given group
 CREATE OR REPLACE FUNCTION get_member_balance(member_id INTEGER, group_id INTEGER)
     RETURNS NUMERIC AS
     $BODY$
-        --- Difference of the amounts to pay and already paid
+        -- Difference of the amounts to pay and already paid
         SELECT COALESCE(share_to_pay_agg.amount, 0.0)
                - COALESCE(expenses_made_agg.amount, 0.0)
                - COALESCE(transfers_made_agg.amount, 0.0)
                + COALESCE(transfers_received_agg.amount, 0.0)
                AS balance
         FROM (
-            --- Aggregation of the total amount of the selected expenses
-            --- weighted by the number of members involved in each expense
+            -- Aggregation of the total amount of the selected expenses
+            -- weighted by the number of members involved in each expense
             SELECT SUM(table_expense.amount / num_members_agg.num_members) AS amount
             FROM (
-                --- Count of the number of members in the expenses
-                SELECT table_expense_member.expense_id, COUNT(table_expense_member.member_id) AS num_members
+                -- Count of the number of members in the expenses
+                SELECT 
+                    table_expense_member.expense_id,
+                    COUNT(table_expense_member.member_id) AS num_members
                 FROM table_expense_member
                 WHERE table_expense_member.expense_id IN (
-                    --- Expense IDs matching the member and the group
+                    -- Expense IDs matching the member and the group
                     SELECT table_expense.id AS expense_id
                     FROM table_expense
-                    INNER JOIN table_expense_member ON table_expense_member.expense_id = table_expense.id
-                    WHERE table_expense.group_id = $2 AND table_expense_member.member_id = $1
+                    INNER JOIN table_expense_member
+                        ON table_expense_member.expense_id = table_expense.id
+                    WHERE table_expense.group_id = $2
+                        AND table_expense_member.member_id = $1
                 )
                 GROUP BY table_expense_member.expense_id
             ) AS num_members_agg
-            --- Link to the expense table based on the expenses for which the number of members was aggregated
-            INNER JOIN table_expense ON num_members_agg.expense_id = table_expense.id
+            -- Link to the expense table based on the expenses for which
+            -- the number of members was aggregated
+            INNER JOIN table_expense
+                ON num_members_agg.expense_id = table_expense.id
         ) AS expenses_made_agg,
         (
-            --- Expenses matching the member and the group, for which the member actually made the expense,
-            --- and aggregation of the total amount
+            -- Expenses matching the member and the group, for which the member actually
+            -- made the expense, and aggregation of the total amount
             SELECT SUM(table_expense.amount) AS amount
             FROM table_expense
-            INNER JOIN table_expense_member ON table_expense_member.expense_id = table_expense.id
-            WHERE table_expense.group_id = $2 AND table_expense_member.member_id = $1 AND table_expense_member.made_expense IS TRUE
+            INNER JOIN table_expense_member
+                ON table_expense_member.expense_id = table_expense.id
+            WHERE table_expense.group_id = $2
+                AND table_expense_member.member_id = $1
+                AND table_expense_member.made_expense IS TRUE
         ) AS share_to_pay_agg,
         (
-            --- Transfers matching the member and the group, for which the member received money,
+            -- Transfers matching the member and the group, for which the member received money,
             SELECT SUM(table_transfer.amount) AS amount
             FROM table_transfer
-            WHERE table_transfer.group_id = $2 AND table_transfer.to_member_id = $1
+            WHERE table_transfer.group_id = $2
+                AND table_transfer.to_member_id = $1
         ) AS transfers_received_agg,
         (
-            --- Transfers matching the member and the group, for which the member paid money,
+            -- Transfers matching the member and the group, for which the member paid money,
             SELECT SUM(table_transfer.amount) AS amount
             FROM table_transfer
-            WHERE table_transfer.group_id = $2 AND table_transfer.from_member_id = $1
+            WHERE table_transfer.group_id = $2
+                AND table_transfer.from_member_id = $1
         ) AS transfers_made_agg
     $BODY$
     LANGUAGE 'sql';
     
 
---- List the balances of all members of a group
+-- List the balances of all members of a group
 DROP TYPE IF EXISTS member_balance_t CASCADE;
 CREATE TYPE member_balance_t AS (member_name VARCHAR, balance numeric);
+
 CREATE OR REPLACE FUNCTION get_group_balances(group_id INTEGER)
     RETURNS SETOF member_balance_t AS
     $BODY$
@@ -188,7 +248,8 @@ CREATE OR REPLACE FUNCTION get_group_balances(group_id INTEGER)
             FOR member IN (
                 SELECT table_member_group.member_id, table_member.name
                 FROM table_member_group
-                INNER JOIN table_member ON table_member_group.member_id = table_member.id
+                INNER JOIN table_member
+                    ON table_member_group.member_id = table_member.id
                 WHERE table_member_group.group_id = $1
             )
             LOOP
@@ -208,14 +269,17 @@ CREATE OR REPLACE FUNCTION get_group_balances(group_id INTEGER)
 --
 
 
---- Create a new group
-CREATE OR REPLACE FUNCTION insert_member(member_name VARCHAR, member_password_hash VARCHAR, member_password_salt VARCHAR)
+-- Create a new group
+CREATE OR REPLACE FUNCTION insert_member(member_name VARCHAR,
+                                         member_password_hash VARCHAR,
+                                         member_password_salt VARCHAR)
     RETURNS INTEGER AS
     $BODY$
         DECLARE
             member_id INTEGER;
         BEGIN
-            INSERT INTO table_member (name, password_hash, password_salt) VALUES ($1, DECODE($2, 'base64'), DECODE($3, 'base64'));
+            INSERT INTO table_member (name, password_hash, password_salt)
+                VALUES ($1, DECODE($2, 'base64'), DECODE($3, 'base64'));
             member_id := (SELECT currval(pg_get_serial_sequence('table_member', 'id')));
             RETURN member_id;
         END
@@ -223,20 +287,25 @@ CREATE OR REPLACE FUNCTION insert_member(member_name VARCHAR, member_password_ha
     LANGUAGE 'plpgsql';
     
     
---- Create a new group
-CREATE OR REPLACE FUNCTION insert_group(name VARCHAR, owner_id INTEGER, other_members VARIADIC INTEGER[])
+-- Create a new group
+CREATE OR REPLACE FUNCTION insert_group(name VARCHAR,
+                                        owner_id INTEGER,
+                                        other_members VARIADIC INTEGER[])
     RETURNS INTEGER AS
     $BODY$
         DECLARE
             group_id INTEGER;
             other_member_id INTEGER;
         BEGIN
-            INSERT INTO table_group (name) VALUES ($1);
+            INSERT INTO table_group (name)
+                VALUES ($1);
             group_id := (SELECT currval(pg_get_serial_sequence('table_group', 'id')));
-            INSERT INTO table_member_group (member_id, group_id, is_owner) VALUES ($2, group_id, TRUE);
+            INSERT INTO table_member_group (member_id, group_id, is_owner)
+                VALUES ($2, group_id, TRUE);
             FOR other_member_id IN (SELECT i FROM UNNEST($3) AS i )
             LOOP
-                INSERT INTO table_member_group (member_id, group_id, is_owner) VALUES (other_member_id, group_id, FALSE);
+                INSERT INTO table_member_group (member_id, group_id, is_owner)
+                    VALUES (other_member_id, group_id, FALSE);
             END LOOP;
             RETURN group_id;
         END
@@ -244,16 +313,18 @@ CREATE OR REPLACE FUNCTION insert_group(name VARCHAR, owner_id INTEGER, other_me
     LANGUAGE 'plpgsql';
     
     
---- Create a new group member
-CREATE OR REPLACE FUNCTION insert_group_member(new_member_id INTEGER, target_group_id INTEGER)
+-- Create a new group member
+CREATE OR REPLACE FUNCTION insert_group_member(new_member_id INTEGER,
+                                               target_group_id INTEGER)
     RETURNS VOID AS
     $BODY$
-        INSERT INTO table_member_group (member_id, group_id, is_owner) VALUES ($1, $2, FALSE);
+        INSERT INTO table_member_group (member_id, group_id, is_owner)
+            VALUES ($1, $2, FALSE);
     $BODY$
     LANGUAGE 'sql';
     
     
---- Remove an expense
+-- Remove an expense
 CREATE OR REPLACE FUNCTION delete_expense(expense_id INTEGER)
     RETURNS VOID AS
     $BODY$
