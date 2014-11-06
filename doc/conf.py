268614b27b34add5ec4ada9ddb5d46e23919f7cf
Dana Christen <dana.christen@gmail.com>
2014-11-05 18:03:48 -0800

    Added module mocking to allow building on readthedocs.org

--- doc/conf.py
+++ doc/conf.py
@@ -15,6 +15,8 @@
 
 import sys
 import os
+from unittest.mock import MagicMock
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -277,3 +279,15 @@ def skip(app, what, name, obj, skip, options):
 
 def setup(app):
     app.connect("autodoc-skip-member", skip)
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['psycopg2']
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if on_rtd:
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
